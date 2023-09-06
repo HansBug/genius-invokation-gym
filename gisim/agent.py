64e1f2a5d2b7dba4ee3f984bbda8db2114f5310e
HansBug <hansbug@buaa.edu.cn>
2023-09-06 13:59:04 +0800

    dev(narugo): add multiple agent

--- gisim/agent.py
+++ gisim/agent.py
@@ -2,7 +2,7 @@
 """
 from abc import ABC
 from collections import Counter
-from typing import Dict, List
+from typing import Dict, List, Iterator
 
 from gisim.cards.characters import get_character_card
 from gisim.classes.action import (
@@ -30,6 +30,11 @@ class Agent(ABC):
     def __init__(self, player_id: PlayerID):
         self.player_id = player_id
 
+    def take_action(self, game_info: GameInfo) -> Action:
+        raise NotImplementedError
+
+
+class SimpleAgent(ABC):
     def take_action(self, game_info: GameInfo) -> Action:
         if game_info.status == GameStatus.INITIALIZING:
             if game_info.phase == GamePhase.CHANGE_CARD:
@@ -94,7 +99,106 @@ class Agent(ABC):
         return DeclareEndAction()
 
 
-class AttackOnlyAgent(Agent):
+class NoActionAssigned(Exception):
+    pass
+
+
+class MultipleActionAgent(Agent):
+    def __init__(self, player_id: PlayerID):
+        Agent.__init__(self, player_id)
+        self._current_iter = None
+
+    def take_multiple_actions(self, game_info: GameInfo) -> Iterator[Action]:
+        raise NotImplementedError
+
+    def take_action(self, game_info: GameInfo) -> Action:
+        try:
+            if self._current_iter is None:
+                raise StopIteration
+
+            return next(self._current_iter)
+        except StopIteration:
+
+            self._current_iter = self.take_multiple_actions(game_info)
+            try:
+                return next(self._current_iter)
+            except StopIteration:
+                raise NoActionAssigned(f'No action assigned for {game_info!r}. '
+                                       f'Please check the implementation of '
+                                       f'{self.__class__.__name__}.take_multiple_action')
+
+
+class GenericMultipleAction(MultipleActionAgent):
+    def yield_actions_on_init_change_card(self, game_info: GameInfo) -> Iterator[Action]:
+        # default behaviour: do nothing
+        _ = game_info
+        yield ChangeCardsAction(cards_idx=[])
+
+    def yield_actions_on_init_select_character(self, game_info: GameInfo) -> Iterator[Action]:
+        # default behaviour: push the middle one up
+        _ = game_info
+        yield ChangeCharacterAction(position=CharPos.MIDDLE, dice_idx=[])
+
+    def yield_actions_on_roll_dice(self, game_info: GameInfo) -> Iterator[Action]:
+        # default behaviour: use the current activate character's element
+        player_info = game_info.get_player_info()
+        current_character = player_info.characters[player_info.active_character_position]
+        character_card = get_character_card(current_character.character.name)
+        character_element = character_card.element_type
+        current_dice = player_info.dice_zone
+        reroll_dice_idx = []
+        for k, element_type in enumerate(current_dice):
+            if element_type not in [character_element, ElementType.OMNI]:
+                reroll_dice_idx.append(k)
+        yield RollDiceAction(dice_idx=reroll_dice_idx)
+
+    def yield_actions_on_play_cards(self, game_info: GameInfo) -> Iterator[Action]:
+        raise NotImplementedError
+
+    def yield_actions_on_round_end(self, game_info: GameInfo) -> Iterator[Action]:
+        # default behaviour: change character when the current one is down
+        player_info = game_info.get_player_info()
+        active_pos = player_info.active_character_position
+        character_info = player_info.characters[active_pos.value]
+        if character_info.character.health_point <= 0:
+            alive_positions = [
+                CharPos(k)
+                for k, character in enumerate(player_info.characters)
+                if character.character.alive
+            ]
+            yield ChangeCharacterAction(
+                position=alive_positions[0], dice_idx=[]
+            )
+        else:
+            yield DeclareEndAction()
+
+    def take_multiple_actions(self, game_info: GameInfo) -> Iterator[Action]:
+        if game_info.status == GameStatus.INITIALIZING:
+            if game_info.phase == GamePhase.CHANGE_CARD:
+                yield from self.yield_actions_on_init_change_card(game_info)
+                return
+
+            elif game_info.phase == GamePhase.SELECT_ACTIVE_CHARACTER:
+                yield from self.yield_actions_on_init_select_character(game_info)
+                return
+
+        elif game_info.status == GameStatus.RUNNING:
+            if game_info.phase == GamePhase.ROLL_DICE:
+                yield from self.yield_actions_on_roll_dice(game_info)
+                return
+
+            elif game_info.phase == GamePhase.PLAY_CARDS:
+                yield from self.yield_actions_on_play_cards(game_info)
+                return
+
+            elif game_info.phase == GamePhase.ROUND_END:
+                yield from self.yield_actions_on_round_end(game_info)
+                return
+
+        yield DeclareEndAction()
+
+
+class AttackOnlyAgent(SimpleAgent):
     def __init__(self, player_id: PlayerID):
         super().__init__(player_id)
 
@@ -365,7 +469,7 @@ class AttackOnlyAgent(Agent):
             )
 
 
-class NoAttackAgent(Agent):
+class NoAttackAgent(SimpleAgent):
     def __init__(self, player_id: PlayerID):
         super().__init__(player_id)
 
